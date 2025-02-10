41c7f0341b72b54c8d6487ac3a5d5246f8289e7a
David Berthiaume <david.berthiaume@avalara.com>
2025-02-09 21:57:59 -0500

    Working on getting debug and release builds working

--- src/main.c
+++ src/main.c
@@ -3,6 +3,17 @@
 #include "core/memory.h"
 #include "platform/platform.h"
 
+char *get_window_title() {
+#define TITLE_SIZE                                                             \
+    (sizeof(APPNAME) + sizeof(APPVERSION) + sizeof(RENDERER) +                 \
+     sizeof(PLATFORM) + sizeof(BUILDDATE) + 9)
+
+    static char title[TITLE_SIZE];
+    snprintf(title, TITLE_SIZE, "%s %s [%s] %s %s: %s", APPNAME, APPVERSION,
+             BUILDDATE, RENDERER, PLATFORM, BUILDTYPE);
+    return title;
+}
+
 int main() {
     init_logger();
     dlog("starting application");
@@ -11,29 +22,10 @@ int main() {
     elog("renderer: %s", RENDERER);
     wlog("platform: %s", PLATFORM);
     ilog("date: %s", BUILDDATE);
-    const int title_size = sizeof(APPNAME) + sizeof(APPVERSION) +
-                           sizeof(RENDERER) + sizeof(PLATFORM) +
-                           sizeof(BUILDDATE) + 9;
-    char title[title_size];
-    snprintf(title, title_size, "%s %s [%s] %s %s: %s", APPNAME, APPVERSION,
-             BUILDDATE, RENDERER, PLATFORM, BUILDTYPE);
 
-    if (!platform_init(1200, 800, title)) {
+    if (!platform_init(1200, 800, get_window_title())) {
         elog("window initialization failed");
         return 1;
-    }
-
-    unsigned char *ptr = (unsigned char *)oalloc(1024, MEMORY_CATEGORY_GAME);
-    if (ptr) {
-        ilog("allocated memory for game: %zu bytes",
-             get_allocated_memory(MEMORY_CATEGORY_GAME));
-
-        ilog("total memory allocated: %zu bytes", get_total_allocated_memory());
-        ofree(ptr);
-        ilog("allocated memory for game after free: %zu bytes",
-             get_allocated_memory(MEMORY_CATEGORY_GAME));
-    } else {
-        printf("failed to allocate memory\n");
     }
 
     while (!platform_should_close()) {
